2ad58d5e570b6009d359aa0956646c6d21b909df
Joel Messerli <hi.github@peg.nu>
2019-10-12 23:17:05 +0200

    Add jwt authentication

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module peg.nu/short
 go 1.12
 
 require (
+	github.com/auth0/go-jwt-middleware v0.0.0-20190805220309-36081240882b // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gorilla/mux v1.7.3
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/urfave/negroni v1.0.0
 )
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/urfave/negroni"
 	"log"
 	"net/http"
 	"os"
 	"peg.nu/short/dao"
 	"peg.nu/short/shortener"
+	"strings"
 	"time"
 )
 
@@ -26,8 +33,74 @@ func main() {
 
 	r.HandleFunc("/{path}", s.RedirectShort).Methods("GET")
 
+	block, _ := pem.Decode([]byte(fmt.Sprintf("-----BEGIN PUBLIC KEY-----\n%v\n-----END PUBLIC KEY-----", os.Getenv("SHORT_JWT_PUBKEY"))))
+	if block == nil {
+		panic("failed to parse pem block containing public key")
+	}
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			// should return *rsa.PublicKey for RS256
+			return pub, nil
+		},
+		SigningMethod:       jwt.SigningMethodRS256,
+		CredentialsOptional: true,
+	})
+
+	n := negroni.New()
+	n.Use(negroni.NewRecovery())
+	n.Use(negroni.NewLogger())
+	n.Use(negroni.HandlerFunc(jwtMiddleware.HandlerWithNext))
+	n.Use(negroni.HandlerFunc(func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		var requiredRole string
+		if strings.HasPrefix(r.URL.Path, "/api/link") {
+			method := r.Method
+
+			if method == http.MethodPost {
+				requiredRole = "PegNu-Short.CREATE"
+			} else if method == http.MethodDelete {
+				requiredRole = "PegNu-Short.DELETE"
+			}
+		}
+
+		// short circuit if no authentication is required
+		if len(requiredRole) == 0 {
+			next(rw, r)
+			return
+		}
+
+		if r.Context().Value("user") == nil {
+			rw.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		token := r.Context().Value("user").(*jwt.Token)
+		resAccess := token.Claims.(jwt.MapClaims)["resource_access"].(map[string]interface{})
+		roles := resAccess["short"].(map[string]interface{})["roles"].([]interface{})
+
+		roleFound := false
+		for _, role := range roles {
+			if role.(string) == requiredRole {
+				roleFound = true
+				break
+			}
+		}
+
+		if !roleFound {
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		next(rw, r)
+	}))
+	n.UseHandler(r)
+
 	srv := &http.Server{
-		Handler:      r,
+		Handler:      n,
 		Addr:         "0.0.0.0:8080",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
